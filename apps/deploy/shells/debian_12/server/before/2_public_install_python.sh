c8bc387d722211b72b53e68706a56a85fe91422c
accountbelongstox <accountbelongstox@163.com>
2024-04-10 21:19:14 +0800

    2024-04-10@21-19-13

--- apps/deploy/shells/debian_12/server/before/2_public_install_python.sh
+++ apps/deploy/shells/debian_12/server/before/2_public_install_python.sh
@@ -4,26 +4,30 @@ DEPLOY_DIR=$(dirname "$DEPLOY_CHIDDIR")
 TOP_DIR=$(dirname "$(dirname "$DEPLOY_DIR")")
 
 current_python_version=$(python3.9 --version 2>&1)
+current_pip_version=$(pip3 --version 2>&1)
 echo "Current Python version: $current_python_version"
+echo "Current Pip version: $current_pip_version"
 
-if [[ $current_python_version != Python\ 3.9* ]]; then
-    echo "Python 3.9 is not the default version. Installing Python 3.9..."
+if [[ $current_python_version != Python\ 3.9* ]] || [[ $current_pip_version != pip\ 21* ]]; then
+    echo "Python 3.9 or Pip 21 is not the default version. Installing Python 3.9 and Pip 21..."
 
     # Install prerequisites
     sudo apt update && sudo apt upgrade
     sudo apt install -y wget build-essential libreadline-gplv2-dev libncursesw5-dev \
-         libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+         libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev \
+         libffi-dev zlib1g-dev openssl libssl-dev libbz2-dev libreadline-dev \
+         libsqlite3-dev llvm libncurses5-dev xz-utils tk-dev
 
     # Download Python source code
-    wget -P /tmp https://www.python.org/ftp/python/3.9.16/Python-3.9.16.tgz
+    sudo wget -P /tmp https://www.python.org/ftp/python/3.9.16/Python-3.9.16.tgz
 
     # Extract archive
-    tar xzf /tmp/Python-3.9.16.tgz -C /tmp
+    sudo tar xzf /tmp/Python-3.9.16.tgz -C /tmp
 
     # Compile Python source
     cd /tmp/Python-3.9.16
-    ./configure --enable-optimizations
-    make altinstall
+    sudo ./configure --enable-optimizations --with-ensurepip=install
+    sudo make altinstall
 
     echo "Python 3.9 installed successfully."
 
@@ -33,9 +37,8 @@ if [[ $current_python_version != Python\ 3.9* ]]; then
     echo "Python 3.9 set as default version."
 else
     echo "Python 3.9 is already installed."
-
-    echo "Python 3.9 is already installed."
 fi
+
 
 if ! which pip3 &> /dev/null; then
     sudo apt install -y python3-pip
@@ -43,7 +46,7 @@ else
     echo "pip3 is already installed."
 fi
 
-python3 --version
+/python3.9 --version
 
 VENV_DIR="$TOP_DIR/venv_linux"
 if [ ! -d "$VENV_DIR" ]; then
