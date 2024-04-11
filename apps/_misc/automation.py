0dc90a94e79ba082f5cdec923f012e58e3c739e6
accountbelongstox <accountbelongstox@163.com>
2024-04-11 16:40:48 +0800

    2024-04-11@16-40-47

--- apps/_misc/automation.py
+++ apps/_misc/automation.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 from pycore.base.requirement_fn.auto_install import auto_install
-=======
-from pycore.base.requirement_fn import auto_install
->>>>>>> aff269c51514af00846548538e9421414a244f97
+
 auto_install.start()
 if __name__ == "__main__":
     from apps.automation.main import auto_main
