3790f771194ef80d98da026451249c87592bc528
accountbelongstox <accountbelongstox@163.com>
2024-04-11 16:41:24 +0800

    2024-04-11@16-41-23

--- apps/_misc/gene_doc.py
+++ apps/_misc/gene_doc.py
@@ -1,9 +1,7 @@
 # from win32com import client as wc
-<<<<<<< HEAD
+
 from pycore.base.requirement_fn.auto_install import auto_install
-=======
-from pycore.base.requirement_fn import auto_install
->>>>>>> aff269c51514af00846548538e9421414a244f97
+
 
 if __name__ == "__main__":
     auto_install.start()
--- apps/_misc/scan_and_read_json.py
+++ apps/_misc/scan_and_read_json.py
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 from pycore.base.requirement_fn.auto_install import auto_install
-=======
-from pycore.base.requirement_fn import auto_install
->>>>>>> aff269c51514af00846548538e9421414a244f97
+
 
 if __name__ == "__main__":
     auto_install.start()
