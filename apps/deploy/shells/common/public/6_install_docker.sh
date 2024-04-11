8f5c41a0df34e021059e60deceaa1d457cf6c186
accountbelongstox <accountbelongstox@163.com>
2024-04-11 15:00:44 +0800

    2024-04-11@15-00-44

--- apps/deploy/shells/common/public/6_install_docker.sh
+++ apps/deploy/shells/common/public/6_install_docker.sh
@@ -52,7 +52,7 @@ centos_9() {
 }
 debian_install() {
     echo "Docker is not installed. Installing..."
-    sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    yes | sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt update
     sudo apt install docker-ce docker-ce-cli containerd.io rsync
