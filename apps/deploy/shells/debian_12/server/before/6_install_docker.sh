d69b38b485661fb7f562b3ca6a1b45305e62fd15
accountbelongstox <accountbelongstox@163.com>
2024-04-11 15:02:30 +0800

    2024-04-11@15-02-29

--- apps/deploy/shells/debian_12/server/before/6_install_docker.sh
+++ apps/deploy/shells/debian_12/server/before/6_install_docker.sh
@@ -27,9 +27,9 @@ install_docker() {
 #
 #    echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 #
-
-    sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
+#
+#    sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     sudo apt-get update
