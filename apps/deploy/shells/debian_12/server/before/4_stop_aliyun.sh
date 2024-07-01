99fd6095d2830dd49beaa0ba9f071198f1e0e774
test <test@qq.com>
2024-07-01 17:33:11 +0800

    2024-07-01@17-33-11

--- apps/deploy/shells/debian_12/server/before/4_stop_aliyun.sh
+++ apps/deploy/shells/debian_12/server/before/4_stop_aliyun.sh
@@ -1,13 +1,11 @@
-
-
 # Check if aliyun.service is running
 if systemctl is-active --quiet aliyun.service; then
     echo "aliyun.service is running. Proceeding with uninstallation."
 
     # Download and execute the uninstallation scripts
-    sudo wget "http://update2.aegis.aliyun.com/download/uninstall.sh" && chmod +x uninstall.sh && ./uninstall.sh
-    sudo wget http://update.aegis.aliyun.com/download/quartz_uninstall.sh && chmod +x quartz_uninstall.sh && ./quartz_uninstall.sh
-    sudo wget http://update.aegis.aliyun.com/download/uninstall.sh && chmod +x uninstall.sh && ./uninstall.sh
+    sudo wget "http://update2.aegis.aliyun.com/download/uninstall.sh" && chmod +x uninstall.sh &&./uninstall.sh
+    sudo wget http://update.aegis.aliyun.com/download/quartz_uninstall.sh && chmod +x quartz_uninstall.sh &&./quartz_uninstall.sh
+    sudo wget http://update.aegis.aliyun.com/download/uninstall.sh && chmod +x uninstall.sh &&./uninstall.sh
 
     # Additional cleanup commands
     echo "Uninstalling Aliyun service..."
@@ -18,43 +16,30 @@ if systemctl is-active --quiet aliyun.service; then
     sudo systemctl disable aliyun.service
     sudo rm -rf /usr/local/share/assist-daemon
     sudo rm -rf /usr/local/share/aliyun-assis
-
-else
-    echo "aliyun.service is not running. No action required."
 fi
 
 if systemctl list-units --full -all | grep -Fq "aliyun"; then
     echo "aliyun.service exists, stopping the service..."
     sudo systemctl stop aliyun
-else
-    echo "aliyun.service does not exist, skipping."
 fi
 
 if systemctl list-units --full -all | grep -Fq "aegis"; then
     echo "aegis.service exists, stopping the service..."
     sudo systemctl stop aegis
-else
-    echo "aegis.service does not exist, skipping."
 fi
 
 if systemctl list-units --full -all | grep -Fq "aliyun"; then
     echo "aliyun.service exists, disabling the service..."
     sudo systemctl disable aliyun
-else
-    echo "aliyun.service does not exist, skipping."
 fi
 
 if systemctl list-units --full -all | grep -Fq "aegis"; then
     echo "aegis.service exists, disabling the service..."
     sudo systemctl disable aegis
-else
-    echo "aegis.service does not exist, skipping."
 fi
 
 SERVICE_FILE="/etc/init.d/aliyun"
 if [ -f "$SERVICE_FILE" ]; then
     echo "aliyun.service exists, stopping the service..."
     sudo systemctl stop aliyun
-else
-    echo "aliyun.service does not exist, skipping."
 fi
