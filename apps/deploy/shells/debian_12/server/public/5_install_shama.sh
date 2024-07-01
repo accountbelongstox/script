7225502099ef02f421682671ba9bd1c81ef51226
test <test@qq.com>
2024-07-01 17:30:02 +0800

    2024-07-01@17-30-01

--- apps/deploy/shells/debian_12/server/public/5_install_shama.sh
+++ apps/deploy/shells/debian_12/server/public/5_install_shama.sh
@@ -1,42 +1,34 @@
-#CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#PARENT_DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
-#pyscript="$PARENT_DIR/pyscript/main.py"
-#SAMBA_ENABLE=$(sudo python3.9 "$pyscript" env get_val "SAMBA_ENABLE")
-#SAMBA_SHARE_NAME=$(sudo python3.9 "$pyscript" env get_val "SAMBA_SHARE_NAME")
-#MAIN_DIR=$(sudo python3.9 "$pyscript" env get_val "MAIN_DIR")
-#SAMBA_SHARE_NAME=$(sudo python3.9 "$pyscript" env get_val "SAMBA_SHARE_NAME")
-#
-#install_samba() {
-#  if ! dpkg -l | grep -q "samba"; then
-#      local distro=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
-#      case "$distro" in
-#          *"Debian"* | *"Ubuntu"*)
-#              echo "Detected Debian/Ubuntu."
-#              if ! dpkg -l | grep -q "samba"; then
-#                  echo "Samba not installed. Installing on Debian/Ubuntu..."
-#                  sudo apt-get update
-#                  sudo apt-get install -y samba
-#              else
-#                  echo "Samba is already installed on Debian/Ubuntu."
-#              fi
-#              ;;
-#          *"CentOS"*)
-#              echo "Detected CentOS."
-#              if ! rpm -q samba &> /dev/null; then
-#                  echo "Samba not installed. Installing on CentOS..."
-#                  sudo yum install -y samba
-#              else
-#                  echo "Samba is already installed on CentOS."
-#              fi
-#              ;;
-#          *)
-#              echo "Unsupported distribution: $distro"
-#              ;;
-#      esac
-#  else
-#      echo "Samba is already installed on Debian/Ubuntu."
-#  fi
-#}
+install_samba() {
+  if ! dpkg -l | grep -q "samba"; then
+      local distro=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+      case "$distro" in
+          *"Debian"* | *"Ubuntu"*)
+              echo "Detected Debian/Ubuntu."
+              if ! dpkg -l | grep -q "samba"; then
+                  echo "Samba not installed. Installing on Debian/Ubuntu..."
+                  sudo apt-get update
+                  sudo apt-get install -y samba
+              else
+                  echo "Samba is already installed on Debian/Ubuntu."
+              fi
+              ;;
+          *"CentOS"*)
+              echo "Detected CentOS."
+              if ! rpm -q samba &> /dev/null; then
+                  echo "Samba not installed. Installing on CentOS..."
+                  sudo yum install -y samba
+              else
+                  echo "Samba is already installed on CentOS."
+              fi
+              ;;
+          *)
+              echo "Unsupported distribution: $distro"
+              ;;
+      esac
+  else
+      echo "Samba is already installed on Debian/Ubuntu."
+  fi
+}
 #setup_samba() {
 #    if [ "$SAMBA_ENABLE" = "yes" ]; then
 #        if ! grep -q "$MAIN_DIR" /etc/samba/smb.conf; then
@@ -59,8 +51,6 @@
 #        fi
 #    fi
 #}
-#install_samba
+install_samba
 #setup_samba
-#
-#
-echo "Skipping shama install."
+
